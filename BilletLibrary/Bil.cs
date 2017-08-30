6ae44f34baca1396d06559b82ac8d3f05da2eede
Jonatan <Jonatan@DESKTOP-3SG699B>
2017-08-30 12:26:36 +0200

    - Opretter "BilletLibrary" med klassen "Bil". - Lavet test over "Bil" klassens metoder.

---
+++ BilletLibrary/Bil.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilletLibrary
+{
+    public class Bil
+    {
+        public string Nummerplade;
+        public DateTime Dato;
+
+        public int Pris()
+        {
+            return 240;
+        }
+
+        public string Køretøj()
+        {
+            return "Bil";
+        }
+    }
+}
---
+++ BilletLibraryTests/BilTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BilletLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilletLibrary.Tests
+{
+    [TestClass()]
+    public class BilTests
+    {
+        [TestMethod()]
+        public void PrisTestBil240()
+        {
+            //Arrange
+            var bil = new Bil();
+            //Act
+            int pris = bil.Pris();
+            //Assert
+            Assert.AreEqual(240,pris);
+        }
+
+        [TestMethod()]
+        public void KøretøjTestBil()
+        {
+            //Arrange
+            var bil = new Bil();
+            //Act
+            string køretøj = bil.Køretøj();
+            //Assert
+            Assert.AreEqual("Bil",køretøj);
+        }
+    }
+}
