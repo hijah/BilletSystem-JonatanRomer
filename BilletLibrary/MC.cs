442cb085ae7603e4218348318664f62a7c039dfb
Jonatan <Jonatan@DESKTOP-3SG699B>
2017-08-30 12:33:18 +0200

    - Oprettet "MC" klassen _ Lavet test over "MC"'s metoder

---
+++ BilletLibrary/MC.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilletLibrary
+{
+    public class MC
+    {
+        public string Nummerplade;
+        public DateTime Dato;
+
+        public int Pris()
+        {
+            return 125;
+        }
+
+        public string Køretøj()
+        {
+            return "MC";
+        }
+    }
+}
---
+++ BilletLibraryTests/MCTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BilletLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilletLibrary.Tests
+{
+    [TestClass()]
+    public class MCTests
+    {
+        [TestMethod()]
+        public void PrisTestMC125()
+        {
+            //Arrange
+            var mc = new MC();
+            //Act
+            int pris = mc.Pris();
+            //Assert
+            Assert.AreEqual(125, pris);
+        }
+
+        [TestMethod()]
+        public void KøretøjTestMC()
+        {
+            //Arrange
+            var mc = new MC();
+            //Act
+            string køretøj = mc.Køretøj();
+            //Assert
+            Assert.AreEqual("MC", køretøj);
+        }
+    }
+}
